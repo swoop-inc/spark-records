412fe5125b61b00b490e01f249ae4af1ce8b714b
MrPowers <matthewkevinpowers@gmail.com>
2020-10-16 08:09:29 -0400

    Remove spark-test-sugar dependency

---
+++ src/test/scala/com/swoop/spark/SparkSessionTestWrapper.scala
@@ -0,0 +1,19 @@
+package com.swoop.spark
+
+import org.apache.spark.sql.SparkSession
+
+trait SparkSessionTestWrapper {
+
+  lazy val spark: SparkSession = {
+    SparkSession
+      .builder()
+      .master("local")
+      .appName("spark-records")
+      .config(
+        "spark.sql.shuffle.partitions",
+        "1"
+      )
+      .getOrCreate()
+  }
+
+}
--- src/test/scala/examples/fancy_numbers/SparkTest.scala
+++ src/test/scala/examples/fancy_numbers/SparkTest.scala
@@ -1,13 +1,14 @@
 package examples.fancy_numbers
 
+import com.swoop.spark.SparkSessionTestWrapper
 import com.swoop.spark.records._
-import com.swoop.spark.test.SparkSqlSpec
 import org.apache.spark.sql.Dataset
 import org.apache.spark.storage.StorageLevel
 
 
-class SparkTest extends ExampleSpec with SparkSqlSpec with TestNegative5To100 {
+class SparkTest extends ExampleSpec with SparkSessionTestWrapper with TestNegative5To100 {
 
+  val sc = spark.sparkContext
   lazy val dc = SimpleDriverContext(sc)
   lazy val jc = dc.jobContext(SimpleJobContext)
   lazy val ds = recordsDataset(-5 to 100, jc)
@@ -15,54 +16,63 @@ class SparkTest extends ExampleSpec with SparkSqlSpec with TestNegative5To100 {
 
   "in an integration test" - {
     implicit val env = FlatRecordEnvironment()
-    val sqlContext = sqlc
-    import sqlContext.implicits._
+    import spark.implicits._
 
     behave like fancyRecordBuilder(records, jc)
 
     "should build records with Spark" in {
       ds.count should be(105)
     }
+
     "should filter error records" in {
       ds.errorRecords.count should be(6)
     }
+
     "should extract data from records" in {
       ds.recordData.count should be(99)
     }
+
     "should extract issues" in {
       ds.allIssues.count should be(8)
       ds.errorIssues.count should be(6)
     }
+
     "should demonstrate issueCounts() output" in {
       ds.issueCounts.show(false)
     }
+
     "should demonstrate errorIssueCounts() output" in {
       ds.errorIssueCounts.show(false)
     }
+
     "should demonstrate messageCounts() output" in {
       ds.messageCounts.show(false)
     }
+
     "should demonstrate errorMessageCounts() output" in {
       ds.errorMessageCounts.show(false)
     }
+
     "should demonstrate errorDetailCounts() output" in {
       ds.errorIssues.errorDetailCounts().show
     }
+
     "should demonstrate unknownErrorDetailCounts() output" in {
       ds.errorIssues.unknownErrorDetailCounts("examples.fancy_numbers").show
     }
+
     "should demonstrate errorDetails() output" in {
       ds.errorIssues.errorDetails().show
     }
+
     "should demonstrate unknownErrorDetails() output" in {
       ds.errorIssues.unknownErrorDetails("examples.fancy_numbers").show
     }
   }
 
   def recordsDataset(numbers: Seq[Int], jc: JobContext): Dataset[FancyNumberRecord] = {
-    val sqlContext = sqlc
-    import sqlContext.implicits._
-    sqlc.createDataset(numbers)
+    import spark.implicits._
+    spark.createDataset(numbers)
       .mapPartitions(inputs => Example.buildRecords(inputs, jc))
       .persist(StorageLevel.MEMORY_ONLY)
   }
